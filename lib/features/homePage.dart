f0f7b1e380878bede90f217702e7aac02e092be5
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-28 16:06:02 +0000

    remove app bar for now

--- lib/features/homePage.dart
+++ lib/features/homePage.dart
@@ -15,9 +15,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
 
         child: Column(
