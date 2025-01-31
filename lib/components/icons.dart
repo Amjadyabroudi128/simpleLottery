e71e78a29259b171c794da499e13ba5edbcbc597
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-01-31 19:15:22 +0000

    icon refresh color

--- lib/components/icons.dart
+++ lib/components/icons.dart
@@ -1,7 +1,8 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:lottery/features/themes/colors.dart';
 
 class myIcons {
-  static Icon refresh = Icon(Icons.refresh_outlined, size: 25, color: Colors.blueAccent,);
+  static Icon refresh = Icon(Icons.refresh_outlined, size: 25, color: myColors.refreshClr);
  }
