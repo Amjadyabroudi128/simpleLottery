32c3e0b1d8a7547a99aec8a66f6299e18b68546a
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-01 18:10:49 +0000

    icon button

---
+++ lib/components/iconBtn.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'icons.dart';
+
+class lotteryBtn extends StatelessWidget {
+  final VoidCallback onPressed;
+  const lotteryBtn({
+    super.key,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: myIcons.refresh,
+      onPressed: onPressed,
+    );
+  }
+}
