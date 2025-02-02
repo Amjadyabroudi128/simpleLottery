67ad96f1b1e69a292d33cf07e7fb4dbd79958a21
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-02 15:07:59 +0000

    container color

--- lib/features/themes/colors.dart
+++ lib/features/themes/colors.dart
@@ -4,4 +4,5 @@ import 'package:flutter/material.dart';
 class myColors {
   static Color textColor = Colors.black;
   static Color refreshClr = Colors.blueAccent;
+  static Color containerClr = Colors.black87;
 }
