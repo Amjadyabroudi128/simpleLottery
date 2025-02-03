2d4c09674b12401a5011c8326f9cfc17c34feca6
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-03 15:50:49 +0000

    sizedBox

--- lib/components/sizedBox.dart
+++ lib/components/sizedBox.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
 
 class spaceBox extends StatelessWidget {
-  final double height;
-  final double width;
-  const spaceBox({super.key, required this.height, required this.width});
+  final double? height;
+  final double? width;
+  const spaceBox({super.key, this.height, this.width});
 
   @override
   Widget build(BuildContext context) {
