14c4e5ff78e6e27ed88c8e1ee573f3fab35a57fd
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-04 14:32:37 +0000

    card color

--- lib/features/themes/colors.dart
+++ lib/features/themes/colors.dart
@@ -5,4 +5,5 @@ class myColors {
   static Color textColor = Colors.black;
   static Color refreshClr = Colors.blueAccent;
   static Color containerClr = Colors.black87;
+  static Color? cardColor = Colors.grey[350];
 }
