d9bb467d643c2e627a00847cd958043040a0cee3
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-04 14:42:22 +0000

    icon theme

---
+++ lib/features/themes/appTheme.dart
@@ -0,0 +1,11 @@
+
+ import 'package:flutter/material.dart';
+import 'package:lottery/components/appSizeHelper.dart';
+
+class appTheme {
+  static final ThemeData myTheme = ThemeData().copyWith(
+    iconTheme: IconThemeData(
+      size: componentSize.iconSize
+    )
+  );
+ }
