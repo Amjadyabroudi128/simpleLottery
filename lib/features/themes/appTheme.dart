39fd7a663a84ddc263745a36f65347c73e601123
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-05 14:33:04 +0000

    card color global

--- lib/features/themes/appTheme.dart
+++ lib/features/themes/appTheme.dart
@@ -2,10 +2,15 @@
  import 'package:flutter/material.dart';
 import 'package:lottery/components/appSizeHelper.dart';
 
+import 'colors.dart';
+
 class appTheme {
   static final ThemeData myTheme = ThemeData().copyWith(
     iconTheme: IconThemeData(
       size: componentSize.iconSize
+    ),
+    cardTheme: CardTheme(
+      color: myColors.cardColor,
     )
   );
  }
