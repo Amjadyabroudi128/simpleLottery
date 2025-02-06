376694541ec3fa46c5e7b9ca2d437a01db2c0aba
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-06 14:24:22 +0000

    go again

--- lib/components/Strings.dart
+++ lib/components/Strings.dart
@@ -3,4 +3,5 @@ class appStrings {
  static String Win = "Congrats! Yow Won";
  static String tryAgain = "Try again next time";
  static String winNumber = "welcome, The winning number is 3";
+ static String Again  = "GO AGAIN";
 }
