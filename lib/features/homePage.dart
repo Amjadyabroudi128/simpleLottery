b72143a6454547a505d1a01d604d3482760ee852
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-10 14:24:51 +0000

    message text

--- lib/features/homePage.dart
+++ lib/features/homePage.dart
@@ -86,20 +86,3 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class textMessage extends StatelessWidget {
-  const textMessage({
-    super.key,
-    required this.message,
-    required this.currentNumber,
-  });
-
-  final String message;
-  final int currentNumber;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(message,style: TextStyle(fontSize: 19,
-        color: currentNumber == 3 ? myColors.winColor : null ),);
-  }
-}
-
