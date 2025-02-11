a99afe5c944b9114f5754eb43f440a55d956f251
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-11 15:28:14 +0000

    message text size

--- lib/features/widgets/messageText.dart
+++ lib/features/widgets/messageText.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lottery/components/styles.dart';
 
+import '../../components/appSizeHelper.dart';
 import '../themes/colors.dart';
 
 class textMessage extends StatelessWidget {
@@ -16,7 +17,7 @@ class textMessage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Text(message,
-      style: textStyles().message(currentNumber == 3 ? myColors.winColor : null, 19,),
+      style: textStyles().message(currentNumber == 3 ? myColors.winColor : null, componentSize.messageFont,),
     );
   }
 }
