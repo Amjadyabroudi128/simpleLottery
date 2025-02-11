82b679e6d8f3b3dea2516159e76b20bee63f2e25
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-11 15:28:05 +0000

    message text size

--- lib/components/appSizeHelper.dart
+++ lib/components/appSizeHelper.dart
@@ -2,7 +2,7 @@
  class componentSize {
   static double iconSize = 25;
   static double radiusSize = 12;
-  static double messageSize = 19;
   static double numbersize = 22;
   static double textSize = 14;
+  static double messageFont = 19;
  }
