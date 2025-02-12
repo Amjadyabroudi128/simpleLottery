fd8b2700948bb4683b78c2f63964896f1c3f74af
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-12 15:15:12 +0000

    TextButton theme data

--- lib/features/widgets/textBtn.dart
+++ lib/features/widgets/textBtn.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../../components/Strings.dart';
-import '../../components/appSizeHelper.dart';
-import '../../components/styles.dart';
-import '../themes/colors.dart';
 
 TextButton buildTextButton(void Function() theLotteryNumber) {
   return TextButton(
