305fff2be78c24df52fb823585b4a03175bd3fd1
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-12 14:51:02 +0000

    textBtn

---
+++ lib/features/widgets/textBtn.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../../components/Strings.dart';
+import '../../components/appSizeHelper.dart';
+import '../../components/styles.dart';
+import '../themes/colors.dart';
+
+TextButton buildTextButton(void Function() theLotteryNumber) {
+  return TextButton(
+    onPressed: theLotteryNumber,
+    child: Text(appStrings.Again,
+        style: textStyles().myText(myColors.again, componentSize.textSize)),
+  );
+}
